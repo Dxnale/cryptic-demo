0d9ab2af33d3f63c1239cdc82d5a1a341095387c
dxnale <daniel.torrealba@alumnos.ipleones.cl>
2025-10-05 20:56:35 +0200

    feature: avances en login (logica + estilos)

--- demoproject/demoproject/views.py
+++ demoproject/demoproject/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'index.html')
+
+@login_required
+def run_demo(request):
+    return render(request, 'run_demo.html')
